9bc7c0919f7c56c7f4868c957820ac9c6dca8054
octalsrc <nicklewchenko92@gmail.com>
2018-04-18 14:56:42 -0600

    Write RepS state monad interface for replicas

--- lib/Replica.hs
+++ lib/Replica.hs
@@ -1,8 +1,10 @@
 module Replica where
 
+import Control.Monad.State
+
 import Data.EventGraph
 
-data Replica g e = Replica (g e)
+data Replica g e = Replica { hist :: g e }
 
 deliver :: (EventGraph g, Ord e) 
         => g e 
@@ -20,3 +22,15 @@ invoke o (Replica g1) = do v <- eval <$> toList g1
                            let (e,a) = o v
                            g2 <- add e g1
                            return (Replica g2,g2,a)
+
+------------------------------------------------------------------------
+
+type RepS g e = StateT (Replica g e) (Resolver g)
+
+deliverM :: (EventGraph g, Ord e) => g e -> RepS g e ()
+deliverM g1 = (put . Replica) =<< (lift . merge g1 . hist) =<< get
+
+invokeM :: (EventGraph g, Ord e, Effect e) => Op e a -> RepS g e (g e,a)
+invokeM o = do (r',g',a) <- (lift . invoke o) =<< get
+               put r'
+               return (g',a)
