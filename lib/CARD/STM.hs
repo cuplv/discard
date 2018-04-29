c9614e301153d6bfb26f98d016c5549d1e87b8ce
octalsrc <nicklewchenko92@gmail.com>
2018-04-28 23:59:49 -0600

    Work on runNode

--- lib/CARD/STM.hs
+++ lib/CARD/STM.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module CARD.STM where
 
@@ -10,6 +12,7 @@ import Control.Concurrent
 import Control.Concurrent.STM
 
 import CARD.Store
+import CARD.EventGraph
 import CARD.Network
 import CARD.Operation
 
@@ -30,3 +33,44 @@ rinvoke o = do cbwrite <- liftIO newBroadcastTChanIO
                 . writeTChan invoker 
                 $ Command o (liftIO . atomically . writeTChan cbwrite))
                liftIO . atomically $ readTChan cbread
+
+-- | Join two channels end-to-end, with some transformation
+joinChans :: (a -> b) -> (TChan a) -> (TChan b) -> IO ThreadId
+joinChans f i o = joinChansIO (return . f) i o
+
+-- | Join two channels end-to-end, performing some IO transformation
+-- in between
+joinChansIO :: (a -> IO b) -> (TChan a) -> (TChan b) -> IO ThreadId
+joinChansIO f i o = do cin <- atomically $ dupTChan i
+                       cout <- atomically $ dupTChan o
+                       let rc = do a <- atomically $ readTChan cin
+                                   b <- f a
+                                   atomically $ writeTChan cout b
+                                   rc
+                       forkIO rc
+
+-- | Iteratively perform IO actions on items read from a channel
+consume :: (a -> IO ()) -> (TChan a) -> IO ThreadId
+consume f i = do cin <- atomically $ dupTChan i
+                 let rc = (atomically $ readTChan cin) >>= f >> rc 
+                 forkIO rc
+
+makeIter :: (MonadIO m) => TChan a -> [m a]
+makeIter c = (liftIO . atomically $ readTChan c) : makeIter c
+
+newTPair :: (MonadIO m) => m (TChan a, TChan a)
+newTPair = do cin <- liftIO newBroadcastTChanIO
+              cout <- liftIO . atomically $ dupTChan cin
+              return (cin,cout)
+
+runNode :: (Ord i, MonadIO m, MonadEG g (Effect s) m)
+        => i
+        -> TChan (g (Effect s)) 
+        -> RFace s g m a m a
+        -> (m () -> IO ())
+        -> m ()
+runNode (rid :: i) (brc :: TChan (g (Effect s))) (act :: RFace s g m a m a) asIO = do 
+  comm <- newTPair 
+  let reqIter = (makeIter undefined) :: [m (Req s g m a)]
+  liftIO . forkIO . asIO $ runReplica rid reqIter
+  return ()
