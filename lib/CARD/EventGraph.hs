d34012f7020728f4699bdc5d90149c6c79728d43
octalsrc <nicklewchenko92@gmail.com>
2018-05-10 21:09:30 -0600

    Partial work on dag zipper

--- lib/CARD/EventGraph.hs
+++ lib/CARD/EventGraph.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module CARD.EventGraph where
 
 import Control.Monad (foldM)
 import Control.Monad.Trans
 import Data.Foldable (foldl')
+import qualified Data.List as List
 
 class EventGraph g e where
   -- | Instantiate an empty event graph 'g' for some type 'e' of
@@ -42,3 +44,50 @@ serialize g = do me <- pop g
                  case me of
                    Nothing -> return []
                    Just (e,g') -> (++ [e]) <$> serialize g'
+
+-- | A zipper for event graphs
+--
+-- This design won't work!  I need to redefine it such that the focus
+-- is a pair of the effect as stored on the left and the effect as
+-- stored on the right.  The reason is that when pulling effects up
+-- into the edge, they be in the left-stored form so that they can be
+-- properly ordered.
+--
+-- The result is that the orderings must always respect the left side
+-- over the right.
+data EGZ g e h = EGZ { egzLeft :: [(g e, e, h e)]
+                     , egzFocus :: (g e, e, h e)
+                     , egzRight :: [(g e, e, h e)]}
+
+maybeTail :: [a] -> Maybe a
+maybeTail as = case as of
+                 [] -> Nothing
+                 a:[] -> Just a
+                 _:as' -> maybeTail as'
+
+dropTail :: [a] -> [a]
+dropTail as = case as of
+                [] -> []
+                a:[] -> []
+                a:as -> a : dropTail as
+
+initEGZ :: (MonadEG g e m, MonadEG h e m)
+        => g e 
+        -> m (Maybe (EGZ g e h))
+initEGZ g = do es <- edge g
+               let es' = map (\(e,g) -> (g,e,empty)) es
+               return (case maybeTail es' of
+                         Just e -> Just $ EGZ (dropTail es') e []
+                         Nothing -> Nothing)
+
+shiftL :: (MonadEG g e m, MonadEG h e m)
+       => EGZ g e h
+       -> m (Maybe (EGZ g e h))
+shiftL (EGZ (e1:es1) (g,e,h) es2) = undefined
+
+shiftR :: (MonadEG g e m, MonadEG h e m)
+       => EGZ g e h
+       -> m (Maybe (EGZ g e h))
+shiftR = undefined
+
+------------------------------------------------------------------------
