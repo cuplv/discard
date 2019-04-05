55d25902af8894992b54475c75c5ee08e537c470
octalsrc <nicklewchenko92@gmail.com>
2019-04-05 17:25:54 -0600

    Work on LogStore

--- lib/Data/LogStore.hs
+++ lib/Data/LogStore.hs
@@ -7,18 +7,34 @@
 
 module Data.LogStore where
 
+import Data.CARD
+import Data.LamportClock
+
+type VClock = LClock
+
+type Root i r = (i,d)
+
+type Entry i e = (i, VClock i, e)
+
 -- | A rooted sequence, possibly stored through a monadic backend.
--- Here, @l@ is the log structure, @r@ is the root node type, @e@ is
--- the entry node type, and @m@ is the backend monad.
-class LogStore l r e m where
-  init :: r -> m (l r e)
-  log :: e -> l r e -> m (l r e)
-  unlog :: l r e -> m (Either r (e, l r e))
+-- Here, @l@ is the log structure, @i@ is the store/replica identity
+-- type, @r@ is the root node type, @e@ is the entry node type, and
+-- @m@ is the backend monad.
+class LogStore l i r e m where
+  init :: Root i r -> m (l i r e)
+  log :: i -> e -> l i r e -> m (l i r e)
+  unlog :: l i r e -> m (Either (Root i r) (Entry i e, l i r e))
 
-newtype ListLog r e = ListLog (r, [e]) deriving (Show,Read,Eq,Ord)
+-- newtype ListLog r e = ListLog (r, [e]) deriving (Show,Read,Eq,Ord)
 
-instance (Monad m) => LogStore ListLog r e m where
-  init r = return $ ListLog (r,[])
-  log e (ListLog (r,es)) = return $ ListLog (r,e:es)
-  unlog (ListLog (r,e:es)) = return $ Right (e, ListLog (r,es))
-  unlog (ListLog (r,[])) = return $ Left r
+-- instance (Monad m) => LogStore ListLog r e m where
+--   init r = return $ ListLog (r,[])
+--   log e (ListLog (r,es)) = return $ ListLog (r,e:es)
+--   unlog (ListLog (r,e:es)) = return $ Right (e, ListLog (r,es))
+--   unlog (ListLog (r,[])) = return $ Left r
+
+
+-- -- | A CARD feed, where @l@ is the log store, @d@ is the CARD, @i@ is
+-- -- the identifier type (for both feeds and replicas), and @m@ is the
+-- -- backend monad.
+-- class (LogStore l (i,d) (i, VClock i, Effect d) m) => Feed l d i m
