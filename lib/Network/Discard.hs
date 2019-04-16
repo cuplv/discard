dd3f249de120d1fe7fba94bf187693c48e9a4d3e
octalsrc <nicklewchenko92@gmail.com>
2019-04-15 23:06:31 -0600

    Plan out Discard interface

--- lib/Network/Discard.hs
+++ lib/Network/Discard.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Network.Discard
   ( module Network.Discard.Broadcast
   , module Network.Discard.Crypto
@@ -6,8 +8,35 @@ module Network.Discard
   , module Network.Discard.RepCard
   ) where
 
+import Control.Lens
+import Control.Monad.State
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+import Data.EventGraph
+import Data.EventGraph.Ipfs
 import Network.Discard.Broadcast  
 import Network.Discard.Crypto
 import Network.Discard.HTTP
 import Network.Discard.Node
 import Network.Discard.RepCard
+
+type L = Edge (IpfsEG PK)
+
+data Discard d = Discard 
+  { _feeds :: Map FeedId (ManagerConn L d)
+  , _ipfsApi :: IpfsEG PK }
+
+makeLenses ''Discard
+
+createFeed :: d -> StateT (Discard d) IO FeedId
+createFeed = undefined
+
+loadFeeds :: FilePath -> StateT (Discard d) IO [FeedId]
+loadFeeds = undefined
+
+seekFeed :: FeedId -> StateT (Discard d) IO (Maybe d)
+seekFeed = undefined
+
+dropFeed :: FeedId -> StateT (Discard d) IO ()
+dropFeed = undefined
