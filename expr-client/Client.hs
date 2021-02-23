3c8c3a3dd16d78fdc8bb8b971d695148e30dc0d7
octalsrc <nicklewchenko92@gmail.com>
2021-02-23 12:21:39 -0700

    Cut special experiment nodes

--- expr-client/Client.hs
+++ expr-client/Client.hs
@@ -39,7 +39,6 @@ main = do
 data ClientConf i = ClientConf
   { clientNetConf :: FilePath
   , clientMix :: ([(Int,String)],String)
-  , clientMixSpecial :: ([(Int,String)],String)
   , clientRate :: Int
   , clientTime :: Int }
 
@@ -48,7 +47,6 @@ confCLI = execParser $
   let parser = ClientConf
         <$> strOption (short 'c' <> help "net conf filepath")
         <*> option auto (long "mix")
-        <*> option auto (long "special-mix" <> help "mix for a single \"special\" node to use")
         <*> option auto (long "rate" <> metavar "MICROSEC")
         <*> option auto (long "time" <> metavar "SEC")
       misc = (fullDesc <> progDesc "Run an experiment")
