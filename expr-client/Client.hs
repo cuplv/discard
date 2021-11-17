46814ea1d41e8b93f47329b1b3ec370fc87b6237
octalsrc <nicklewchenko92@gmail.com>
2021-11-16 19:35:12 -0700

    Update experiment client

--- expr-client/Client.hs
+++ expr-client/Client.hs
@@ -20,8 +20,6 @@ import Data.List (genericLength)
 import System.Exit
 import Data.Foldable (fold)
 
-import Lang.Carol
-import Lang.Carol.Bank
 import Network.Discard
 import Network.Discard.Experiment
 import Network.Discard.Experiment2
@@ -29,30 +27,31 @@ import Data.EventGraph.Ipfs
 
 main :: IO ()
 main = do 
-  conf <- execParser (info (subparser (command "latency" conf1CLI 
-                                       <> command "stock" conf2CLI)
+  conf <- execParser (info (subparser (command "stock" conf2CLI)
                             <**> helper) (fullDesc <> progDesc "Experiment client"))
   net <- decodeFileEither (clientNetConf conf) >>= \case
     Right net -> return net
     Left _ -> die "Could not read net conf file"
-  case conf of
-    Client1Conf _ _ _ _ _ _ -> runExperiment 
-                                 (Left $ ExpConf (clientRate conf) "asdf" (uncurry Mix (clientMix conf)) (clientTime conf) (clientBatchSize conf) (clientUseTokens conf))
-                                 net
-    Client2Conf _ _ _ _ _ _ _ -> runExperiment
-                                  (Right $ Exp2Conf (clientUseRes conf) (clientTime conf) False (clientWarehouseSize conf) (clientRate conf) (clientBatchSize conf) (clientUseTokens conf))
-                                  net
+  runExperiment 
+    (Exp2Conf
+       { e2UseReservations = clientUseRes conf
+       , e2Time = clientTime conf
+       , e2Primary = "TODO"
+       , e2Others = ["TODO1"]
+       , e2WarehouseSize = clientWarehouseSize conf
+       , e2Rate = clientRate conf
+       , e2BatchSize = clientBatchSize conf
+       , e2UseTokens = clientUseTokens conf
+       })
+    net
+  -- case conf of
+  --   -- Client1Conf _ _ _ _ _ _ -> runExperiment 
+  --   --                              (Left $ ExpConf (clientRate conf) "asdf" (uncurry Mix (clientMix conf)) (clientTime conf) (clientBatchSize conf) (clientUseTokens conf)) net
+  --   Client2Conf _ _ _ _ _ _ _ -> runExperiment
+  --                                 (Exp2Conf (clientUseRes conf) (clientTime conf) False (clientWarehouseSize conf) (clientRate conf) (clientBatchSize conf) (clientUseTokens conf)) net
 
 data ClientConf i = 
-    Client1Conf
-      { clientNetConf :: FilePath
-      , clientMix :: ([(Int,String)],String)
-      , clientRate :: Int
-      , clientTime :: Int
-      , clientBatchSize :: Int
-      , clientUseTokens :: Bool
-      }
-  | Client2Conf
+    Client2Conf
       { clientNetConf :: FilePath
       , clientWarehouseSize :: Int
       , clientRate :: Int
@@ -62,17 +61,17 @@ data ClientConf i =
       , clientUseTokens :: Bool
       }
 
--- conf1CLI :: IO (ClientConf String)
-conf1CLI =
-  let parser = Client1Conf
-        <$> strOption (short 'c' <> help "net conf filepath")
-        <*> option auto (long "mix" <> help "operation mix to use")
-        <*> option auto (long "rate" <> metavar "MICROSEC")
-        <*> option auto (long "time" <> metavar "SEC")
-        <*> option auto (long "batch-size" <> metavar "OPS" <> help "number of ops to batch before broadcast" <> value defaultBatchSize)
-        <*> switch (long "use-tokens" <> help "use token-passing for locks")
-      misc = (fullDesc <> progDesc "Run latency experiment, measuring average op latency")
-  in info (parser <**> helper) misc
+-- -- conf1CLI :: IO (ClientConf String)
+-- conf1CLI =
+--   let parser = Client1Conf
+--         <$> strOption (short 'c' <> help "net conf filepath")
+--         <*> option auto (long "mix" <> help "operation mix to use")
+--         <*> option auto (long "rate" <> metavar "MICROSEC")
+--         <*> option auto (long "time" <> metavar "SEC")
+--         <*> option auto (long "batch-size" <> metavar "OPS" <> help "number of ops to batch before broadcast" <> value defaultBatchSize)
+--         <*> switch (long "use-tokens" <> help "use token-passing for locks")
+--       misc = (fullDesc <> progDesc "Run latency experiment, measuring average op latency")
+--   in info (parser <**> helper) misc
 
 -- conf2CLI :: IO (ClientConf String)
 conf2CLI =
@@ -87,16 +86,19 @@ conf2CLI =
       misc = (fullDesc <> progDesc "Run stock experiment, counting total successful sales")
   in info (parser <**> helper) misc
 
-runExperiment :: Either ExpConf Exp2Conf -> ExpNetConf String -> IO ()
+runExperiment :: Exp2Conf -> ExpNetConf String -> IO ()
 runExperiment ec enc = do
   let nc = getNetConf enc
       addrs = expAddrs enc
+      primary = head (Map.keys addrs)
+      others = tail (Map.keys addrs)
       -- cmd = Launch (divRate enc ec) nc
       cmd = undefined
-      cmds = case ec of
-               Left e1c -> repeat $ Launch (Left $ divRate enc e1c) nc
-               Right e2c -> Launch (Right $ e2c { e2Restocker = True }) nc 
-                            : repeat (Launch (Right $ e2c { e2Restocker = False }) nc)
+      cmds = repeat (Launch (ec { e2Primary = primary, e2Others = others}) nc)
+      -- cmds = case ec of
+      --          -- Left e1c -> repeat $ Launch (Left $ divRate enc e1c) nc
+      --          e2c -> Launch (Right $ e2c { e2Restock = True }) nc 
+      --                 : repeat (Launch (Right $ e2c { e2Restocker = False }) nc)
   man <- newManager defaultManagerSettings
   let mkReq :: (ExpCmd String,String) -> IO (Int,String)
       mkReq (cmd,addr) = do
@@ -110,7 +112,7 @@ runExperiment ec enc = do
   exps <- traverse mkReq (zip cmds (Map.elems addrs))
   putStrLn "All nodes started experiment." >> hFlush stdout
   threadDelay $ (getTime ec + 20) * 1000000
-  let getRes :: (Int,String) -> IO (Either ExpResult Exp2Result)
+  let getRes :: (Int,String) -> IO (Exp2Result)
       getRes (n,addr) = do
         ireq <- parseRequest addr
         let req = ireq { requestBody = RequestBodyLBS $ encode (Report n :: ExpCmd String) }
@@ -120,19 +122,19 @@ runExperiment ec enc = do
                                   Just d -> return d
             | otherwise -> print c >> die "Oops"
   results <- case ec of
-    Left _ -> Left . fold . map unLeft <$> traverse getRes exps
-    Right _ -> Right . fold . map unRight <$> traverse getRes exps
+    -- Left _ -> Left . fold . map unLeft <$> traverse getRes exps
+    _ -> fold <$> traverse getRes exps
   case (ec,results) of
-    (Left ec,Left results) -> do
-      putStrLn $ "* Individual latencies"
-      mapM_ (\(s,l) -> putStrLn $ s ++ ": " ++ show l ++ " s") (Map.assocs (expAvgLatencies results))
-      putStrLn $ "* Failures"
-      print $ expUnfinished results
-      putStrLn $ "* Total measures"
-      putStrLn $ "Total requests: " ++ show (totalReqs results) ++ " req"
-      putStrLn $ "True rate: " ++ show (trueReqRate ec results) ++ " req/s"
-      putStrLn $ "Avg latency: " ++ show (combinedAvgLatency results) ++ " s"
-    (Right ec, Right results) -> do
+    -- (Left ec,Left results) -> do
+    --   putStrLn $ "* Individual latencies"
+    --   mapM_ (\(s,l) -> putStrLn $ s ++ ": " ++ show l ++ " s") (Map.assocs (expAvgLatencies results))
+    --   putStrLn $ "* Failures"
+    --   print $ expUnfinished results
+    --   putStrLn $ "* Total measures"
+    --   putStrLn $ "Total requests: " ++ show (totalReqs results) ++ " req"
+    --   putStrLn $ "True rate: " ++ show (trueReqRate ec results) ++ " req/s"
+    --   putStrLn $ "Avg latency: " ++ show (combinedAvgLatency results) ++ " s"
+    (ec, results) -> do
       putStrLn $ "Total sales: " ++ show (e2TotalSales results)
 
 unLeft (Left a) = a
